11e4c939634d96a3b5d94b5e9b8dfb7f9008f3ee
Denis Carriere <carriere.denis@gmail.com>
2022-02-20 11:16:13 -0500

    update foo example to Typescript

---
+++ examples/foo/foo.cpp
@@ -0,0 +1,31 @@
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract]] foo : public contract {
+public:
+   using contract::contract;
+
+   struct [[eosio::table]] data {
+      name        owner;
+      int64_t     value;
+
+      uint64_t primary_key() const { return owner.value; }
+   };
+
+   typedef multi_index<"data"_n, data> data_index;
+
+   [[eosio::action]]
+   void store(name owner, int64_t value)
+   {
+      require_auth(owner);
+
+      check(value >= 0, "require non-negative value");
+
+      data_index di( get_self(), get_self().value);
+      di.emplace(_self, [&](auto& d) {
+         d.owner = owner;
+         d.value = value;
+      });
+   }
+};
